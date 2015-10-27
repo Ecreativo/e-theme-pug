7583626da319a87b944d8fa5c07bf49aa152f1bd
Camiloruiiz <camiloruiz@esquemacreativo.com>
2015-10-27 15:03:31 +0100

    New trihito

--- Gemfile
+++
@@ -1,5 +0,0 @@
-source "https://rubygems.org"
-
-gem 'sass', '>= 3.3'
-gem 'scss-lint', '~> 0.31.0'
-gem 'fontcustom', '~> 1.3.7'
--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-/*
-* Dependencias 
-* npm install --save-dev gulp gulp-connect-php browser-sync gulp-concat gulp-replace gulp-imagemin gulp-minify-css gulp-concat-css gulp-notify gulp-rename gulp-autoprefixer gulp-uncss gulp-uglify gulp-imagemin
-*/
-
-var requireDir = require('require-dir');
-
-// Require all tasks in gulp/tasks, including subfolders
-requireDir('./gulp/tasks', { recurse: true });
