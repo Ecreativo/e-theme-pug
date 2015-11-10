89eb65453513e01e59e9aaf88925840e39ba0d81
Camiloruiiz <camiloruiz@esquemacreativo.com>
2015-11-10 09:07:10 +0100

    Modify _metas, combine _mixins

--- public_html/_includes/php/_metas.php
+++ public_html/_includes/php/_metas.php
@@ -7,6 +7,6 @@
 	    $meta_descripcion = 'Contáctenos para poder asesorarlo.';
 		}else{
 		$meta_titulo = 'dev.com';
-	    $meta_descripcion = 'dev.com | Somos, nos encantaría saber de usted.';
+	    $meta_descripcion = 'Somos dev.com | nos encantaría saber de usted.';
 	}
 ?>
