5cb240ac9d5db22a9166de29176401db449b82ce
Camiloruiiz <camiloruiz@esquemacreativo.com>
2016-12-04 11:32:34 +0000

    Reorganizing

--- app/_includes/_conn.php
+++
@@ -1,4 +0,0 @@
-<?php
-	//$con = mysqli_connect("$db_server","$db_user","$db_password","$db_name")or die("DB Error");
-	//mysqli_query($con,"SET NAMES utf8");
-?>
--- app/_includes/_footer.php
+++
@@ -1,5 +0,0 @@
-<hr>
-<footer>
-    <p>&copy; Company <?php echo date('Y'); ?> © <strong>Otra estrategia de Esquema creativo</strong>.</p>
-</footer>
-<?php require_once('_includes/_js.php'); ?>
