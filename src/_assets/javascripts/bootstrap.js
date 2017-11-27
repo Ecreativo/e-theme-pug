405e48e3b6979b1e10090d0d52f485d0c2593a5c
Camiloruiiz <camiloruiz@esquemacreativo.com>
2017-11-27 17:28:02 +0000

    using boostrap partials from dist folder

--- src/_assets/javascripts/bootstrap.js
+++ src/_assets/javascripts/bootstrap.js
@@ -1,16 +1,36 @@
+window.$ = window.jQuery = require('jquery')
+window.Popper = require('popper.js')
+
 // If you don't have ES6 transpiler or have TypeScript then you could use distributed version but will loose module customization
 //import 'bootstrap/dist/js/bootstrap';
 
-// If you have ES6 transpiler then you could code below and will be able to customize what modules will be included in the build.
 
+// import all bootstrap
+//require('bootstrap')
+
+// import plugins individually
+import 'bootstrap/js/dist/util';
+import 'bootstrap/js/dist/modal';
+import 'bootstrap/js/dist/tooltip';
+import 'bootstrap/js/dist/popover';
+
+// If you have ES6 transpiler then you could code below and will be able to customize what modules will be included in the build.
 //import 'bootstrap/js/src/alert';
-import 'bootstrap/js/src/button';
+//import 'bootstrap/js/src/button';
 //import 'bootstrap/js/src/carousel';
-import 'bootstrap/js/src/collapse';
-import 'bootstrap/js/src/dropdown';
+//import 'bootstrap/js/src/collapse';
+//import 'bootstrap/js/src/dropdown';
 //import 'bootstrap/js/src/modal';
 //import 'bootstrap/js/src/popover';
 //import 'bootstrap/js/src/scrollspy';
 //import 'bootstrap/js/src/tab';
 //import 'bootstrap/js/src/tooltip';
-import 'bootstrap/js/src/util';
+//import 'bootstrap/js/src/util';
+
+//import '../scss/main.scss'
+
+// use tooltip and popover components everywhere
+$(function (){
+  $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="popover"]').popover()
+})
