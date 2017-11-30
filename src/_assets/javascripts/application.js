b54ded18ccc343269a70ed460384a882415da6d9
Camiloruiiz <camiloruiz@esquemacreativo.com>
2017-11-30 17:12:46 +0000

    [fixes] eslint errors

--- src/_assets/javascripts/application.js
+++ src/_assets/javascripts/application.js
@@ -1,43 +1,44 @@
-(function($, window, document, undefined) {
+import Flickity from 'flickity';
 
-  'use strict';
+(function ($, window, document, undefined) {
+  'use strict'
 
-  function cargar() {
-    $('.loader').fadeIn();
-  }
+  // function cargar() {
+  //   $('.loader').fadeIn()
+  // }
 
   function descargar() {
-    $('.loader').fadeOut();
+    $('.loader').fadeOut()
   }
 
-  $(window).on('load', function() {
-    descargar();
-  });
+  $(window).on('load', function () {
+    descargar()
+  })
   /* ==============================================
   GO TO TOP
   =============================================== */
 
-  jQuery(document).ready(function($) {
-    $(window).scroll(function() {
+  jQuery(document).ready(function ($) {
+    $(window).scroll(function () {
       if ($(this).scrollTop() > 50) {
-        $('.back-to-top').fadeIn('slow');
+        $('.back-to-top').fadeIn('slow')
       } else {
-        $('.back-to-top').fadeOut('slow');
+        $('.back-to-top').fadeOut('slow')
       }
-    });
-    $('.back-to-top').click(function() {
-      $("html, body").animate({
+    })
+    $('.back-to-top').click(function () {
+      $('html, body').animate({
         scrollTop: 0
-      }, 500);
-      return false;
-    });
-    $('.go-to-top').click(function() {
-      $("html, body").animate({
+      }, 500)
+      return false
+    })
+    $('.go-to-top').click(function () {
+      $('html, body').animate({
         scrollTop: 0
-      }, 500);
-      return false;
-    });
-  });
+      }, 500)
+      return false
+    })
+  })
 
   /* ==============================================
   Flickity
@@ -56,13 +57,12 @@
         pageDots: false,
         lazyLoad: true,
         imagesLoaded: true
-      };
+      }
 
-      var sliderElem = document.querySelector('.slider');
-      const $mainSlider = new Flickity(sliderElem, options);
+      var sliderElem = document.querySelector('.slider')
+      const $mainSlider = new Flickity(sliderElem, options)
+      return $mainSlider
     }
   }
   $twitterSlider()
-
-
-})(jQuery, window, document);
+})(jQuery, window, document)
--- src/_assets/javascripts/bootstrap.js
+++ src/_assets/javascripts/bootstrap.js
@@ -1,38 +1,33 @@
-window.$ = window.jQuery = require('jquery')
-window.Popper = require('popper.js')
-
 // If you don't have ES6 transpiler or have TypeScript then you could use distributed version but will loose module customization
-//import 'bootstrap/dist/js/bootstrap';
-
+// import 'bootstrap/dist/js/bootstrap'
 
 // import all bootstrap
-//require('bootstrap')
-
-// import plugins individually
-import 'bootstrap/js/dist/dropdown';
-import 'bootstrap/js/dist/modal';
-import 'bootstrap/js/dist/popover';
-import 'bootstrap/js/dist/tooltip';
-import 'bootstrap/js/dist/util';
-
+// require('bootstrap')
 
 // If you have ES6 transpiler then you could code below and will be able to customize what modules will be included in the build.
-//import 'bootstrap/js/src/alert';
-//import 'bootstrap/js/src/button';
-//import 'bootstrap/js/src/carousel';
-//import 'bootstrap/js/src/collapse';
-//import 'bootstrap/js/src/dropdown';
-//import 'bootstrap/js/src/modal';
-//import 'bootstrap/js/src/popover';
-//import 'bootstrap/js/src/scrollspy';
-//import 'bootstrap/js/src/tab';
-//import 'bootstrap/js/src/tooltip';
-//import 'bootstrap/js/src/util';
+// import 'bootstrap/js/src/alert'
+// import 'bootstrap/js/src/button'
+// import 'bootstrap/js/src/carousel'
+// import 'bootstrap/js/src/collapse'
+// import 'bootstrap/js/src/dropdown'
+// import 'bootstrap/js/src/modal'
+// import 'bootstrap/js/src/popover'
+// import 'bootstrap/js/src/scrollspy'
+// import 'bootstrap/js/src/tab'
+// import 'bootstrap/js/src/tooltip'
+// import 'bootstrap/js/src/util'
+
+// import plugins individually
+import 'bootstrap/js/dist/dropdown'
+import 'bootstrap/js/dist/modal'
+import 'bootstrap/js/dist/popover'
+import 'bootstrap/js/dist/tooltip'
+import 'bootstrap/js/dist/util'
 
 import '../scss/main.scss'
 
 // use tooltip and popover components everywhere
-$(function (){
+$(function () {
   $('[data-toggle="tooltip"]').tooltip()
   $('[data-toggle="popover"]').popover()
 })
