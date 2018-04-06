f06a4eb5ad0b553de4ec6abb1fc962caccc960b5
Camiloruiiz <camiloruiz@esquemacreativo.com>
2018-04-06 18:20:28 +0100

    [add] todo in loader

--- _assets/javascripts/loader.js
+++ _assets/javascripts/loader.js
@@ -18,3 +18,27 @@ export default function $loader() {
     $loader.out()
   })
 }
+
+// todo cover loader to a class with a construtor.
+// example of construtor
+// class Entry {
+//   constructor(el) {
+//       this.DOM = {
+//           el: el
+//       };
+//       this.init();
+//   }
+//   init() {
+//       // DOM elements:
+//       // title
+//       this.DOM.title = this.DOM.el.querySelector('.section__content > .section__title');
+//       charming(this.DOM.title);
+//       this.DOM.titleLetters = this.DOM.title.querySelectorAll('span');
+//       // description
+//       this.DOM.description = this.DOM.el.querySelector('.section__content > .section__description');
+//       // image
+//       this.DOM.image = this.DOM.el.querySelector('.section__img > .section__img-inner');
+//       // more box
+//       this.DOM.more = this.DOM.el.querySelector('.section__more > .section__more-inner');
+//   }
+// }
