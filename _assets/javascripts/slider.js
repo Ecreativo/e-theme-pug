1ce10acae1dac3b47e847a55b0b4f8f0aca7d4df
Camiloruiiz <camiloruiz@esquemacreativo.com>
2018-07-21 11:08:55 +0100

    [fix] bug with slider [add] bglazyload

--- _assets/javascripts/slider.js
+++ _assets/javascripts/slider.js
@@ -1,48 +1,50 @@
-import Flickity from 'flickity'
-
+import Flickity from 'flickity-bg-lazyload'
+require('flickity-imagesloaded')
 /* ==============================================
   Flickity
   =============================================== */
-export default function $slider(slider) {
+export default function $slider() {
   if ($('.slider').length) {
-    // Main slider options
-    let options = {
-      cellSelector: '.slide',
-      accessibility: false,
-      cellAlign: 'center',
-      draggable: true,
-      autoPlay: true,
-      wrapAround: true,
-      prevNextButtons: true,
-      pageDots: false,
-      lazyLoad: true,
-      imagesLoaded: true
-    }
+    $(window).on('load', function() {
+      // Main slider options
+      let options = {
+        cellSelector: '.slide',
+        accessibility: false,
+        cellAlign: 'center',
+        draggable: true,
+        autoPlay: true,
+        wrapAround: true,
+        prevNextButtons: true,
+        pageDots: false,
+        bgLazyLoad: true,
+        imagesLoaded: true
+      }
 
-    /* eslint-disable */
-    // disable/enable options adapting to different screen sizes
-    if (matchMedia('screen and (max-width: 375px)').matches) {
-      // options.prevNextButtons = false
-    }
+      /* eslint-disable */
+        // disable/enable options adapting to different screen sizes
+        if (matchMedia('screen and (max-width: 375px)').matches) {
+          // options.prevNextButtons = false
+        }
 
-    if (matchMedia('screen and (min-width: 576px)').matches) {
-    }
+        if (matchMedia('screen and (min-width: 576px)').matches) {}
 
-    if (matchMedia('screen and (min-width: 768px)').matches) {
-    }
+        if (matchMedia('screen and (min-width: 768px)').matches) {}
 
-    if (matchMedia('screen and (min-width: 992px)').matches) {
-    }
+        if (matchMedia('screen and (min-width: 992px)').matches) {}
 
-    if (matchMedia('screen and (min-width: 1200px)').matches) {
-    }
+        if (matchMedia('screen and (min-width: 1200px)').matches) {}
 
-    if (matchMedia('screen and (min-width: 1480px)').matches) {
-    }
-    /* eslint-enable */
+        if (matchMedia('screen and (min-width: 1480px)').matches) {}
+        /* eslint-enable */
 
-    let sliderElem = document.querySelector('.slider')
-    let slider = new Flickity(sliderElem, options)
-    return slider
+      let sliderElem = document.querySelector('.slider')
+      // show
+      sliderElem.classList.remove('d-none')
+      // trigger redraw for transition
+      sliderElem.offsetHeight // eslint-disable-line
+
+      let slider = new Flickity(sliderElem, options)
+      return slider
+    })
   }
 }
